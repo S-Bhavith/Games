1cd3ee65fea859fe9bbc319bba8351bd2ea6c94a
Bhavith.S <bhaviths20@gmail.com>
2024-10-30 23:13:31 +0530

    Generating random 1 to 9 numbers inside the block

--- Sudoku/script.js
+++ Sudoku/script.js
@@ -1,17 +1,54 @@
 let container = document.getElementById('container');
-
-// for(let i=0; i<81; i++){
-    
-//     let button = document.createElement('input');
-//     button.id = i;
-//     container.appendChild(button);
-// }
+let row = 0;
 
 for(let i = 0; i<9; i++){
     let subContainer = document.getElementById(`sub-container${i+1}`);
-    for(let j = 0; j<9; j++){
-        let button = document.createElement('input');
-        button.id = j;
-        subContainer.appendChild(button);
+    row++;
+    for(let j = 1; j<10; j++){
+        let input = document.createElement('input');
+        input.id = `${j}${row}`;
+        input.placeholder = `${j}${row}`;
+        subContainer.appendChild(input);
+    }
+}
+
+// Generating Random values
+
+let i = 0,j=0;
+
+const rows = [];
+for(let i = 0; i < 9; i++){
+    rows[i] = [];
+    for(let j=0; j<9; j++){
+        rows[i][j] = document.getElementById(`${j+1}${i+1}`);
+    }
+}
+
+console.log(rows);
+
+let checkRow = [
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9]
+]
+
+function genRan(blockIndex){
+    if(checkRow[blockIndex] != null){
+        let randomIndex = Math.floor(Math.random()*checkRow[blockIndex].length);
+        let ran = checkRow[blockIndex][randomIndex];
+        checkRow[blockIndex].splice(randomIndex,1);
+        return ran;
+    }
+}
+
+for(let i=0; i<9; i++){
+    for(let j=0; j<9; j++){
+        rows[i][j].value = genRan(i);
     }
 }
